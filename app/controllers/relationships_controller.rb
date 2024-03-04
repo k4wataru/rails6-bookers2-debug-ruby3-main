0eda16a498b98c49e48e0e2eb9a4b6571421c756
EC2 Default User <ec2-user@ip-172-31-32-108.ap-northeast-1.compute.internal>
2024-03-04 13:33:49 +0000

    フォロー機能実装修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,35 +1,24 @@
 class RelationshipsController < ApplicationController
-  def follow(user)   # フォローするとき
-    relationships.create(followed_id: user.id)
-  end
-  
-  def unfollow(user)   # フォローを外すとき
-    relationships.find_by(followed_id: user.id).destroy
-  end
-
-  def following?(user)   # フォローしているか確認するとき
-    following.include?(user)
-  end
-  
-  def create   # フォロー追加機能
+  before_action :authenticate_user!
+  def create
     user = User.find(params[:user_id])
     current_user.follow(user)
     redirect_to request.referer
   end
   
-  def destroy   # フォロー削除機能
+  def destroy
     user = User.find(params[:user_id])
     current_user.unfollow(user)
     redirect_to request.referer
   end
-   
-  def followers   # フォロワー一覧表示
+  
+  def followers
     user = User.find(params[:user_id])
     @users = user.followers
   end
 
-  def following   # フォロー一覧表示
+  def followings
     user = User.find(params[:user_id])
-    @users = user.following
+		@users = user.followings
   end
 end
